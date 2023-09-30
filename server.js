b3a9ea5586c44a4304f0d91f82ca558e03829a11
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-09-30 02:02:04 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
